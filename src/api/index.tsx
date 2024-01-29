cdabf70adee00f88b6fc4a9babf54a82a34eb7db
prodiensheikh <hugh.mungus0201@gmail.com>
2024-01-29 22:55:30 +0530

    run scripts

--- src/api/index.tsx
+++ src/api/index.tsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const BASE_URL = "https://slick-squids-dance.loca.lt";
+export const BASE_URL = import.meta.env.VITE_BASE_URL;
 
 export const getRequest = async (url: string) => {
   return await axios.get(`${BASE_URL}/${url}`, {
